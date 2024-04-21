57ec9e0a7b4113ec87629ce571c0d483cce28085
Anja Kuzevska <anjakuzevska@gmail.com>
2024-04-21 10:01:25 +0200

    env file, email confirmation and authentication

--- Program.cs
+++ Program.cs
@@ -4,22 +4,27 @@ using Cinder.Data;
 using Microsoft.AspNetCore.Identity;
 using Cinder.Models;
 using Cinder.Seeders;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using dotenv.net;
+
+DotEnv.Load(); 
 
 var builder = WebApplication.CreateBuilder(args);
+
+var sendGridApiKey = Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+var client = new SendGridClient(sendGridApiKey);
+
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext") ?? throw new InvalidOperationException("Connection string 'ApplicationContext' not found.")));
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-.AddRoles<IdentityRole>()
-.AddEntityFrameworkStores<ApplicationContext>();
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationContext>();
 
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddSassCompiler();
-
-// Add SignalR for messaging
 builder.Services.AddSignalR();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
@@ -31,50 +36,30 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddScoped<MessageService>(); 
 builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddSingleton<ISendGridClient>(client);
 
 var app = builder.Build();
 
-
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
-    var context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationContext>();
     DbInitializer.Initialize(context);
-}
-
-using(var scope = app.Services.CreateScope())
-{
-    var serviceProvider = scope.ServiceProvider;
-    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
-    var context = serviceProvider.GetRequiredService<ApplicationContext>();
-
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    
     await UserSeeder.SeedRolesAsync(roleManager);
     LanguageSeeder.SeedAllLanguages(context);
     FacultySeeder.SeedAllFaculties(context);
     HobbySeeder.SeedAllHobbies(context);
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseCors("CorsPolicy");
-app.MapHub<ChatHub>("/chatHub");
-
+app.UseExceptionHandler("/Home/Error");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-app.UseAuthentication();;
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapRazorPages();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
