15125f17bb135fd573399717f305db0977dd6ffe
Aleksa Sibinović <97167895+A-Sibi@users.noreply.github.com>
2024-04-21 09:10:44 +0200

    add recommendation system
    * initial reccomendation commit
    
    * rework
    
    * app_testing data samples
    
    * added all atributes
    
    * basic scoring system
    
    * added hobies
    
    * ready to recieve data
    
    * Open AI API call for bio similarity
    
    * changed index of df, finished functions
    
    * rest api
    
    * json format fixed
    
    * patched working matching
    
    * del migrations
    
    * existing user fix
    
    * working id + matching (OpenAI off)
    
    * Matching algorithm -> Ready for Production
    
    * Merging Recomendation System into Main
    
    * temp
    
    ---------
    
    Co-authored-by: Anastasija <anastasija@pc7.dev>
    Co-authored-by: Anja Kuzevska <anjakuzevska@gmail.com>

---
+++ Dtos/FacultyDto.cs
@@ -0,0 +1,9 @@
+namespace Cinder.Dtos
+{
+    public class FacultyDto
+    {
+        public int Id_Faculty { get; set; }
+        public string? Name { get; set; }
+        // Other relevant properties
+    }
+}
